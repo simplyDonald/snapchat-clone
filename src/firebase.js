10414b1229e60eb6c3638a20cd444d661fafd7ce
simplyDonald <donaldabuah@gmail.com>
2022-12-18 01:01:12 -0500

    setup firebase functionalities

--- src/firebase.js
+++ src/firebase.js
@@ -9,3 +9,11 @@ const firebaseConfig = {
   appId: "1:79927973096:web:28a377e4dcc73192c4e55d",
   measurementId: "G-0R85ZLS30Q"
 };
+
+const firebaseApp = firbase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export {db, auth, storage, provider}
