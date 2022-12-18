f4923fa56a62b4c67da2680645036b6735f0c161
simplyDonald <donaldabuah@gmail.com>
2022-12-18 01:22:44 -0500

    setup webcam

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import WebcamCapture from './components/WebcamCapture'
 
 function App() {
   return (
     <div className="app">
-      Snapchat proceed build now
+      <h1>Snapchat proceed build now</h1>
+      <WebcamCapture />
     </div>
   );
 }
