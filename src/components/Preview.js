7ff107c4feeb40620501355b00b9274537e3b8e6
simplyDonald <donaldabuah@gmail.com>
2022-12-19 23:48:12 -0500

    setup functiona;ity to grab image from redux store, redirect to  index on empty inmg store & add icons from material ui

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,10 +1,63 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { resetCameraImage, selectCameraImage } from '../features/cameraSlice'
 import './Preview.css'
+import CloseIcon from '@mui/icons-material/Close';
+import TextFieldsIcon from '@mui/icons-material/TextFields';
+import CreateIcon from '@mui/icons-material/Create';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import CropIcon from '@mui/icons-material/Crop';
+import TimerIcon from '@mui/icons-material/Timer';
+import NotesIcon from '@mui/icons-material/Notes';
+import SendIcon from '@mui/icons-material/Send';
+
 
 function Preview() {
+
+   const cameraImage = useSelector(selectCameraImage)
+
+   const navigate = useNavigate();
+
+   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if(!cameraImage){
+      navigate('/', {replace: true})
+    }
+
+  },[cameraImage, navigate])
+
+  const closePreview = () => {
+    dispatch(resetCameraImage())
+  }
+
+  const sendPost = () => {
+
+    
+  }
+
   return (
     <div className="preview">
-      <h1>preview here duh...</h1>
+      <CloseIcon className='preview__close' onClick={closePreview} />
+      <div className="preview__toolbarRight">
+        <TextFieldsIcon />
+        <CreateIcon />
+        <NotesIcon />
+        <MusicNoteIcon />
+        <AttachFileIcon />
+        <CropIcon />
+        <TimerIcon />
+
+      </div>
+      <div onClick={sendPost} className="preview__footer">
+        <h2>Send Now</h2>
+        <SendIcon className="preview__sendIcon" />
+      </div>
+
+      <img src={cameraImage} alt="" />
+
     </div>
   )
 }
