8a2cbb5a074f6348a2328cccc2e08b88724946af
simplyDonald <donaldabuah@gmail.com>
2022-12-20 01:51:14 -0500

    setup individual chat component with styling

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,19 @@
+import { Avatar } from '@mui/material'
 import React from 'react'
 import './Chat.css'
+import StopRoundedIcon from '@mui/icons-material/StopRounded';
 
-function Chat() {
+function Chat({id, username, timestamp, imageUrl, read, profilePic}) {
   return (
-   <div className="chat">
+  <div className="chat">
+    <Avatar className="chat__avatar" src={profilePic} />
+    <div className="chat__info">
+      <h4>{username}</h4>
+      <p>Tap to view - {new Date(timestamp?.toDate()).toUTCString()}</p>
+    </div>  
 
-   </div>
+    {!read && <StopRoundedIcon className='chat__readIcon' />}
+  </div>
   )
 }
 
