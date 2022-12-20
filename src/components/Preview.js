d7d1a1a960c0ae9b066c2cecdf8c784b80d71c4a
simplyDonald <donaldabuah@gmail.com>
2022-12-20 00:35:19 -0500

    setup functionality for uploading puictures to storage and cxreating posts in firestore

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -12,6 +12,9 @@ import CropIcon from '@mui/icons-material/Crop';
 import TimerIcon from '@mui/icons-material/Timer';
 import NotesIcon from '@mui/icons-material/Notes';
 import SendIcon from '@mui/icons-material/Send';
+import { v4 as uuid } from 'uuid';
+import { db, storage } from '../firebase';
+import firebase from 'firebase';
 
 
 function Preview() {
@@ -34,8 +37,29 @@ function Preview() {
   }
 
   const sendPost = () => {
+    // cretaes a random unique id:
+    const id = uuid();
 
-    
+    // create a reference to the storage location:
+    const uploadTask = storage.ref(`posts/${id}`).putString(cameraImage, "data_url");
+
+    uploadTask.on('state_changed', null, (error) => {
+      // error function
+      console.log(error)
+    }, () => {
+      // complete function
+      storage.ref('posts').child(id).getDownloadURL().then(url => {
+        db.collection('posts').add({
+          imageUrl: url,
+          username: 'johndoe',
+          read: false,
+          profilePic: 'https://www.w3schools.com/howto/img_avatar.png',
+          timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        })
+        navigate('/chats', {replace: true})
+      })
+
+    });
   }
 
   return (
