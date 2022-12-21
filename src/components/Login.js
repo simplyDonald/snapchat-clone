8e35c106cc46c6bae8199c16b1e94ab47c083b39
simplyDonald <donaldabuah@gmail.com>
2022-12-20 21:51:20 -0500

    add lkogin page and logic

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,38 @@
+import { Button } from '@mui/material'
 import React from 'react'
+import './Login.css'
+import { auth, provider } from '../firebase';
+import { useDispatch } from 'react-redux';
+import { login } from '../features/appSlice';
 
 function Login() {
+
+  const dispatch = useDispatch()
+
+  const signIn = () => {
+    auth.signInWithPopup(provider).then(
+      ({displayName,photoURL,uid}) => {
+        dispatch(login({
+          username: displayName,
+          profilePic: photoURL,
+          id: uid
+        }))
+      }
+    )
+    .catch((error) => alert(error.message))
+  }
+
+
   return (
    <div className="login">
-    
+    <div className="login__container">
+      <img src="https://scx2.b-cdn.net/gfx/news/2017/1-snapchat.jpg" alt="" />
+      <Button variant='outlined' onClick={signIn}>
+        Sign In
+      </Button>
+
+    </div>
+
    </div>
   )
 }
