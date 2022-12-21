338fe2d92e5c8fb29ff637073e2f704865baaad1
simplyDonald <donaldabuah@gmail.com>
2022-12-20 21:50:47 -0500

    setup user persistence logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 
@@ -6,13 +6,28 @@ import WebcamCapture from './components/WebcamCapture'
 import Preview from './components/Preview';
 import Chats from './components/Chats';
 import ChatView from './components/ChatView';
-import { selectUser } from './features/appSlice';
+import { login, logout, selectUser } from './features/appSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import Login from './components/Login';
+import { auth } from './firebase';
 
 function App() {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(login({
+          username: authUser.displayName,
+          profilePic: authUser.photoURL,
+          id: authUser.uid
+        }))
+      } else {
+        dispatch(logout())
+      }
+    })
+  }, [dispatch])
 
   return (
     <div className="app">
@@ -24,7 +39,7 @@ function App() {
                 <Route path="/preview" element={<Preview />} />
                 <Route path="/chats" element={<Chats />} />
                 <Route path="/chats/view" element={<ChatView />} />
-                <Route path="*" element={<div>Hi</div>} />
+                <Route path="*" element={<div><h2>hello npeople</h2></div>} />
               </Routes>
             </div>
         </BrowserRouter>
