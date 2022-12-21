4df6b2199df6fec2c91f5a5d5603a1001ea4043f
simplyDonald <donaldabuah@gmail.com>
2022-12-20 21:09:57 -0500

    complete functionality and styling for chats

--- src/components/ChatView.js
+++ src/components/ChatView.js
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { selectSelectedImage } from '../features/appSlice'
 import './ChatView.css'
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 
 function ChatView() {
+  const selectedImage = useSelector(selectSelectedImage)
+  const navigate = useNavigate();
+  const exit = useCallback(() => {
+    navigate('/chats', {replace: true})
+
+  },[navigate])
+
+  useEffect(() => {
+    if (!selectedImage) {
+      exit()
+    }
+  },[selectedImage, exit])
+
   return (
     <div className="chatView">
-      <h2>chatview dey here</h2>
+      <img src={selectedImage} alt="" onClick={exit} />
+      <div className="chatView__timer">
+        <CountdownCircleTimer
+          isPlaying
+          duration={10}
+          strokeWidth={6}
+          size={50} 
+          colors={[
+            ['#004777', 0.33],
+            ['#F7B801', 0.33],
+            ['#A30000', 0.33],
+          ]}
+        >
+          {({remainingTime}) => {
+            if (remainingTime === 0) {
+              exit()
+            }
+            return remainingTime
+          }}
+        </CountdownCircleTimer>  
+      </div>  
     </div>
   )
 }
