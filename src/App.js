fdea88ece31bd4d6735a676bd2805ee6825572cd
simplyDonald <donaldabuah@gmail.com>
2022-12-20 21:09:27 -0500

    add login ternary

--- src/App.js
+++ src/App.js
@@ -6,21 +6,30 @@ import WebcamCapture from './components/WebcamCapture'
 import Preview from './components/Preview';
 import Chats from './components/Chats';
 import ChatView from './components/ChatView';
+import { selectUser } from './features/appSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import Login from './components/Login';
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
   return (
     <div className="app">
-      <BrowserRouter>
-        <div className="app__body">
-          <Routes>
-            <Route path="/" element={      <WebcamCapture />} />
-            <Route path="/preview" element={<Preview />} />
-            <Route path="/chats" element={<Chats />} />
-            <Route path="/chats/view" element={<ChatView />} />
-            <Route path="*" element={<div>Hi</div>} />
-          </Routes>
-        </div>
+      {!user ? (<Login />) :(
+          <BrowserRouter>
+            <div className="app__body">
+              <Routes>
+                <Route path="/" element={      <WebcamCapture />} />
+                <Route path="/preview" element={<Preview />} />
+                <Route path="/chats" element={<Chats />} />
+                <Route path="/chats/view" element={<ChatView />} />
+                <Route path="*" element={<div>Hi</div>} />
+              </Routes>
+            </div>
         </BrowserRouter>
+      )}
+      
     </div>
   );
 }
